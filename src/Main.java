fdd580c1801f015f3fb38b9e770f5840894b9b50
Dens <dadadadenns1@gmail.com>
2026-03-25 19:48:29 +1000

    new!!!!!

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Привет Мир!!!!!");
         System.out.println("Hello, World!!!!!");
-        System.out.println("HI world!");
+        System.out.println("Hello, Hello");
     }
 }
